d6c98d9f01b84777ac686f629e6f84baf529a9d7
thainole <thainole@gmail.com>
2021-06-23 12:34:11 -0500

    try catch on login

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,11 +3,30 @@ const axios = require('axios');
 const jwtDecode = require('jwt-decode')
 
 export const getToken = async (requestToken) => {
-  try {
+/*   try { */
       const resp = await axios.post('http://localhost:3001/auth', requestToken);
-      const decodeToken = jwtDecode.default(resp.data.token)
-      return decodeToken;
-  } catch (err) {
-      console.error(err);
-  }
+      
+      console.log(resp, 'hola')
+
+      switch (resp.status) {
+        case 200:
+          const decodeToken = jwtDecode.default(resp.data.token)
+          return decodeToken;
+        case 400:
+          throw new Error('Inserta un email y contraseña correctos')
+        default:
+          throw new Error(resp.statusText)
+      }
+      // return decodeToken;
+  /* } catch (err) {
+    console.log(err) */
+/*       switch (key) {
+        case value:
+          
+          break;
+      
+        default:
+          break;
+      } 
+  }*/
 }
