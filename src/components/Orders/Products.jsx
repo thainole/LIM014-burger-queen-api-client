b966eab6163c8811dc40d3fbaea4eccd7af127b5
thainole <thainole@gmail.com>
2021-06-23 11:08:59 -0500

    get token and products layout

--- src/components/Orders/Products.jsx
+++ src/components/Orders/Products.jsx
@@ -1,82 +1,20 @@
 import React from 'react'
-// import { productsRequest } from '../../services/products'
+import { productsRequest } from '../../services/products'
 
 export const Products = () => {
 
-  const productos = {
-    "products": [
-      {
-        "_id": "005",
-        "name": "Coffee with milk",
-        "price": "7",
-        "type": "drink",
-        "image": "https://i.pinimg.com/564x/92/c6/28/92c628ef8827ff77ee25df2b142d66e5.jpg",
-      },
-      {
-        "_id": "007",
-        "name": "Burger",
-        "price": "15",
-        "type": "burger",
-        "image": "https://i.pinimg.com/564x/6b/6a/06/6b6a06f2e77150adc71aa8e717e6e16d.jpg",
-      },
-      {
-        "_id": "005",
-        "name": "Coffee with milk",
-        "price": "7",
-        "type": "drink",
-        "image": "https://i.pinimg.com/564x/92/c6/28/92c628ef8827ff77ee25df2b142d66e5.jpg",
-      },
-      {
-        "_id": "007",
-        "name": "Burger",
-        "price": "15",
-        "type": "burger",
-        "image": "https://i.pinimg.com/564x/6b/6a/06/6b6a06f2e77150adc71aa8e717e6e16d.jpg",
-      },
-      {
-        "_id": "005",
-        "name": "Coffee with milk",
-        "price": "7",
-        "type": "drink",
-        "image": "https://i.pinimg.com/564x/92/c6/28/92c628ef8827ff77ee25df2b142d66e5.jpg",
-      },
-    ]
-  }
-  const data = productos.products
+  const [state, setState] = React.useState([])
 
-  /* const firstView = data.filter((elem) => elem.type === "Desayuno");
-  const [products, setProducts] = useState(firstView);
+   productsRequest().then(res => setState(res.products))
 
-  const productsType = (option) => {
-    // eslint-disable-next-line default-case
-    switch (option) {
-      case "Desayuno":
-        const breakfast = data.filter((elem) => elem.type === option);
-        setProducts(breakfast);
-        break;
-      case "Hamburguesas":
-        const burgers = data.filter((elem) => elem.type === option);
-        setProducts(burgers);
-        break;
-      case "Acompañamientos":
-        const sides = data.filter((elem) => elem.type === option);
-        setProducts(sides);
-        break;
-      case "Bebidas":
-        const drinks = data.filter((elem) => elem.type === option);
-        setProducts(drinks);
-        break;
-    }
-  }; */
-
-
+  
   return (
     <section className="containerProd">
       <h5>Hi soy productitos</h5>
-      <article className="d-flex flexWrap ">
-          {data.map((product) => (
-            <div  className="card cardsWidth m-2" width="9rem">  
-              <img src={product.image} className="card-img-top imgCardSize" alt="" /* height="50" *//>
+       <article className="d-flex flexWrap ">
+          {state.map((product) => (
+            <div className="card cardsWidth m-2" width="9rem" key={product.id}>  
+              <img src={product.image} className="card-img-top imgCardSize" alt="" />
               <div className="card-body p-1 text-center">
                 <p className="m-auto fw-bold">{product.name}</p>
                 <p className="m-auto">S/. {product.price}</p>
--- src/services/products.js
+++ src/services/products.js
@@ -3,17 +3,7 @@ const axios = require('axios');
 export const productsRequest = async () => {
   try {
       const resp = await axios.get('http://localhost:3001/products');
-      console.log(resp.data)
-      console.log(JSON.parse(resp.data))
       return resp.data
-      /* let strObj = resp.data
-      const jsonStr = strObj.replace(/(\w+:)|(\w+ :)/g, function(s) {
-        return '"' + s.substring(0, s.length-1) + '":';
-      });
-      const hola = JSON.parse(jsonStr)
-      console.log(resp.data)
-      console.log(hola)
-      return hola; */
   } catch (err) {
       console.error(err);
   }
