3c69ff28401db3184e96d6edb2d376a02882acd1
unknown <yrem.vera.5@gmail.com>
2021-07-01 21:48:38 -0500

    Feat sum &  qty

---
+++ src/services/orders.js
@@ -0,0 +1,23 @@
+// const axios = require('axios');
+
+// export const postOrder = async (storedToken, obj) => {
+//     const resp = await axios({
+//       method: 'post',
+//       url: 'https://appi-burger-queen-client.herokuapp.com/products',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${storedToken}`,
+//       },
+//       data: obj
+//     });
+//     switch (resp.status) {
+//       case 200:
+//         return resp.data;
+//       case 400:
+//         throw new Error('no se indica `userId` o se intenta crear una orden sin productos'); 
+//       case 401:
+//         throw new Error('si no hay cabecera de autenticación');      
+//       default:
+//         throw new Error(resp.statusText);
+//     } 
+//   }
